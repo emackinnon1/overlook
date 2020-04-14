f7fb79cc49be94ca45229f830a327fa06121f48a
emackinnon1 <emackinnon100390@gmail.com>
2020-04-14 14:08:59 -0600

    Fetch is working, data is being pulled into variables and in the future will be classes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,51 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+/* 
+Iteration Uno
+set up promise.all
+make userRepo function
+login welcome window
+
+
+
+class structure
+users
+
+	
+
+*/
+$('window').on('load', retrieveAllData)
+
+let roomsData;
+let bookingsData;
+let roomServicesData;
+let usersData;
+
+function retrieveAllData() {
+	Promise.all([
+			fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms").then(response => response.json()),
+			fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings").then(response => response.json()),
+			fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices").then(response => response.json()),
+			fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users").then(response => response.json())
+		])
+		.then(data => makeHotel(data[0].rooms, data[1].bookings, data[2].roomServices, data[3].users))
+		.catch(error => console.log(error));
+}
+
+function makeHotel(rooms, bookings, roomServices, users) {
+	roomsData = rooms;
+	bookingsData = bookings;
+	roomServicesData = roomServices;
+	usersData = users;
+}
+
+setTimeout(print, 300)
+
+function print() {
+	console.log(roomsData)
+	console.log(bookingsData)
+	console.log(roomServicesData)
+	console.log(usersData)
+}
